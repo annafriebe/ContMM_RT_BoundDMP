87a6efa8ba7de49285dd1604c125dbdd02268dc0
anna.friebe@mdh.se <anna.friebe@mdh.se>
2023-03-17 14:44:38 +0100

    storing PROSIT output for auto figure generation

--- eval/PROSIT/run_cli_solver.sh
+++ eval/PROSIT/run_cli_solver.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 
-./cli_solver -R -T 200 -t 40 -q 6 -d 10 -m 6 -C ../../data/models/discr/em_distr6 -M ../../data/models/discr/tr_mat6.txt
-./cli_solver -R -T 200 -t 50 -q 7 -d 8 -m 6 -C ../../data/models/discr/em_distr6 -M ../../data/models/discr/tr_mat6.txt
-./cli_solver -R -T 200 -t 50 -q 8 -d 8 -m 6 -C ../../data/models/discr/em_distr6 -M ../../data/models/discr/tr_mat6.txt
+./cli_solver -R -T 200 -t 40 -q 6 -d 10 -m 6 -C ../../data/models/discr/em_distr6 -M ../../data/models/discr/tr_mat6.txt > "../../data/prosit/result_60000_5.txt"
+./cli_solver -R -T 200 -t 50 -q 7 -d 8 -m 6 -C ../../data/models/discr/em_distr6 -M ../../data/models/discr/tr_mat6.txt > "../../data/prosit/result_70000_4.txt"
+./cli_solver -R -T 200 -t 50 -q 8 -d 8 -m 6 -C ../../data/models/discr/em_distr6 -M ../../data/models/discr/tr_mat6.txt > "../../data/prosit/result_80000_4.txt"
